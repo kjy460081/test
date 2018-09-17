ddd27c3f31dd6eddaca31d29c5044e1c180cb43d
jykimidr <jykimidr@jykimidr-PC>
2018-09-17 16:32:13 +0900

    6

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -8,6 +8,7 @@ public class Test {
 		System.out.println("Hello World!");
 		System.out.println("Hello World!");
 		System.out.println("Hello World!");
+		System.out.println("Hello World!");
 	}
 
 }
