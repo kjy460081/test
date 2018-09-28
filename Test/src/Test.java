08828dfbea7939a36f95b0cd171bcb0232095fd6
kjy46 <kjy46@jykim>
2018-09-28 16:02:42 +0900

    merge

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -3,6 +3,16 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("this is jykim1");
+		System.out.println("this is jykim2");
+		System.out.println("this is jykim1");
+		System.out.println("this is jykim2");
 		System.out.println("this is 1");
 		System.out.println("this is 1");
 	}
