20cbdc17283702a259015240743ab2eeb2882b48
kjy46 <kjy46@jykim>
2018-09-28 15:54:34 +0900

    master commit from jykim2

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -11,6 +11,9 @@ public class Test {
 		System.out.println("Hello World!");
 		System.out.println("this is jykim1");
 		System.out.println("this is jykim2");
+		System.out.println("this is jykim1");
+		System.out.println("this is jykim2");
+		
 	}
 
 }
