bcad9c9fbabbce89b09bfcd4bd9a6098a2167b6b
kjy46 <kjy46@jykim>
2018-09-28 15:52:17 +0900

    jykim 1

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -3,15 +3,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
-		System.out.println("Hello World!");
-		System.out.println("Hello World!");
-		System.out.println("Hello World!");
-		System.out.println("Hello World!");
-		System.out.println("Hello World!");
-		System.out.println("this is jykim1");
-		System.out.println("this is jykim2");
-		System.out.println("this is jykim1");
+		System.out.println("this is 1");
 	}
 
 }
